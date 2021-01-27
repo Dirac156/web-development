970bed96ad4d83fa1dc35a1f6f13c33689cdf986
= <=>
2021-01-27 06:23:10 +0200

    commit all

--- Drum _Kit _Starting_Files/index.js
+++ Drum _Kit _Starting_Files/index.js
@@ -1,8 +1,58 @@
-// document.querySelector("button").addEventListener("click", handleClick);
+// function that will play the sound
+function playSound(letter){
+    switch(letter){
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default:
+            console.log(this);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
 
 var lst = document.querySelectorAll(".drum");
 for (var i = 0; i < lst.length; i++){
     lst[i].addEventListener("click", function(){
-        alert("I got clicked");
+        var buttonInnerHtml = this.innerHTML;
+        playSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
     });
 }
+
+document.addEventListener("keydown", function(event){
+    var key_pressed = event.key;
+    playSound(key_pressed);
+    buttonAnimation(key_pressed);
+})
